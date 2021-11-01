0ea19c485ca81595297f258f5fc483c2c99988cf
JatinBumbra <jatin.bumbra.23@gmail.com>
2021-11-01 16:38:48 +0530

    Create TransactionPool class

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const request = require('request');
+const Wallet = require('./wallet');
 const Blockchain = require('./blockchain');
+const TransactionPool = require('./wallet/transaction-pool');
 const Pubsub = require('./app/pubsub');
 // Init app
 const app = express();
 const blockchain = new Blockchain();
+const wallet = new Wallet();
+const transactionPool = new TransactionPool();
 const pubsub = new Pubsub({ blockchain });
 // Port declaration
 const DEFAULT_PORT = 5000;
@@ -28,6 +32,32 @@ app.post('/api/mine', (req, res) => {
   pubsub.broadcastChain();
 
   res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+  const { amount, recipient } = req.body;
+
+  let transaction = transactionPool.existingTransaction({
+    inputAddress: wallet.publicKey,
+  });
+
+  try {
+    if (transaction) {
+      transaction.update({ senderWallet: wallet, recipient, amount });
+    } else {
+      transaction = wallet.createTransaction({ recipient, amount });
+    }
+  } catch (error) {
+    return res.status(400).json({ type: 'error', message: error.message });
+  }
+
+  transactionPool.setTransaction(transaction);
+
+  res.json({ type: 'success', transaction });
+});
+
+app.get('/api/transaction-pool-map', (req, res) => {
+  res.json(transactionPool.transactionMap);
 });
 
 const syncChains = () => {
